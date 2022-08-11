d40a34d0027cb950bf24e4de52c899d27e3edf10
GrizzlyAtom <111004814+GrizzlyAtom@users.noreply.github.com>
2022-08-11 11:15:28 -0700

    update to personal defaults

--- settings/broadcast.lua
+++ settings/broadcast.lua
@@ -1,5 +1,5 @@
 ------------------------------------------------------------------
 -- Configuration options for scripted systems in this pack
 ------------------------------------------------------------------
-CONFIG.BROADCAST_MAP_DIRECTION = 1
-CONFIG.BROADCAST_ALTERNATE_LAYOUT = 1
+CONFIG.BROADCAST_MAP_DIRECTION = 2.0
+CONFIG.BROADCAST_ALTERNATE_LAYOUT = 3.0
