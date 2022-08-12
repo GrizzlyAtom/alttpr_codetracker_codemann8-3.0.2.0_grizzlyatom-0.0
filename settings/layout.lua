98639aeb1744be5671e457b0c915cd0faa3bf51f
GrizzlyAtom <111004814+GrizzlyAtom@users.noreply.github.com>
2022-08-11 19:07:25 -0700

    Update layout.lua

--- settings/layout.lua
+++ settings/layout.lua
@@ -1,6 +1,6 @@
 ------------------------------------------------------------------
 -- Configuration options for scripted systems in this pack
 ------------------------------------------------------------------
-CONFIG.LAYOUT_ENABLE_ALTERNATE_DUNGEON_VIEW = false
+CONFIG.LAYOUT_ENABLE_ALTERNATE_DUNGEON_VIEW = true
 CONFIG.LAYOUT_USE_THIN_HORIZONTAL_PANE = true
 CONFIG.LAYOUT_ROOM_SLOT_METHOD = 1
